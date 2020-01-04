61135bfe3b88e25f7d3aa316462a3aecbb74c181
Matthew Kennedy <matthewkennedy@outlook.com>
2020-01-04 05:02:22 -0800

    here's how I'd do it

--- firmware/controllers/gauges/tachometer.cpp
+++ firmware/controllers/gauges/tachometer.cpp
@@ -16,92 +16,74 @@
 
 EXTERN_ENGINE;
 
+static scheduling_s events[12];
 
-static scheduling_s tachTurnSignalOff;
-static scheduling_s tachToggleSignalOff;
-static scheduling_s tachToggleSignalOn;
-static angle_t nextEventAngle;
-/* used to detect configuration changes */
-static uint8_t pulseNumbers;
+struct tach_ctx {
+	OutputPin *Pin;
+	bool State;
+};
 
-static void toggleTachPinHigh(DECLARE_ENGINE_PARAMETER_SIGNATURE);
-static void toggleTachPinLow(DECLARE_ENGINE_PARAMETER_SIGNATURE);
+static tach_ctx ctx_high;
+static tach_ctx ctx_low;
 
-static void turnTachPinLow(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-	enginePins.tachOut.setLow();
+static void setTach(void *param) {
+	auto ctx = reinterpret_cast<tach_ctx *>(param);
+	ctx->Pin->setValue(ctx->State);
 }
-
-static void toggleTachPinLow(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-
-	enginePins.tachOut.setLow();
-	if (1 < engineConfiguration->tachPulsePerRev){
-		scheduleByAngle(&tachToggleSignalOn, nextEventAngle,
-					(schfunc_t) &toggleTachPinHigh, engine PASS_ENGINE_PARAMETER_SUFFIX);
-	}
-
-
-}
-
-static void toggleTachPinHigh(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-
-	enginePins.tachOut.setHigh();
-	if (1 < engineConfiguration->tachPulsePerRev){
-		scheduleByAngle(&tachToggleSignalOff, nextEventAngle,
-							(schfunc_t) &toggleTachPinLow, engine PASS_ENGINE_PARAMETER_SUFFIX);
-
-	}
-}
-
 
 static void tachSignalCallback(trigger_event_e ckpSignalType,
 		uint32_t index DECLARE_ENGINE_PARAMETER_SUFFIX) {
 
+	// TODO: does index 0 mean the beginning of a revolution, or of a cycle?
+
+	// only process at index 0 - we schedule the full revolution all at once
+	if (index != (uint32_t)engineConfiguration->tachPulseTriggerIndex) {
+		return;
+	}
+
 #if EFI_UNIT_TEST
-	printf("tachSignalCallback(%d %d)\n",ckpSignalType,index);
+	printf("tachSignalCallback(%d %d)\n", ckpSignalType, index);
 #else
 	UNUSED(ckpSignalType);
 #endif
 
-	if (1 < engineConfiguration->tachPulsePerRev) {
-		if (pulseNumbers != engineConfiguration->tachPulsePerRev){
-			if (0 == index) {
+	// TODO: warning if periods is set too high
 
-				enginePins.tachOut.setHigh();
-				pulseNumbers = engineConfiguration->tachPulsePerRev;
+	// How many tach pulse periods do we have?
+	const int periods = engineConfiguration->tachPulsePerRev;
+	// What is the angle per tach output period?
+	const angle_t period = 360.0 / periods;
 
-				nextEventAngle = 360/(engineConfiguration->tachPulsePerRev*2);
-				scheduleByAngle(&tachToggleSignalOff, nextEventAngle,
-    						(schfunc_t) &toggleTachPinLow, engine PASS_ENGINE_PARAMETER_SUFFIX);
+	float duty;
 
-			}
-			else
-			{
-				return;
-			}
+	if (engineConfiguration->tachPulseDurationAsDutyCycle) {
+		// Simple case - duty explicitly set
+		duty = engineConfiguration->tachPulseDuractionMs;
+	} else {
+		// Constant high-time mode - compute the correct duty cycle
+		float cycleTimeMs = 60000.0 / GET_RPM();
+		float periodTimeMs = cycleTimeMs / periods;
 
-		}
-		else
-		{
-			return;
-		}
-
+		duty = engineConfiguration->tachPulseDuractionMs / periodTimeMs;
 	}
-	else
-	{
-		if (index != (uint32_t)engineConfiguration->tachPulseTriggerIndex) {
-			return;
-		}
-		pulseNumbers = 1;
-		enginePins.tachOut.setHigh();
-		float durationMs;
-		if (engineConfiguration->tachPulseDurationAsDutyCycle) {
-			// todo: implement tachPulseDurationAsDutyCycle
-			durationMs = engineConfiguration->tachPulseDuractionMs;
-		} else {
-			durationMs = engineConfiguration->tachPulseDuractionMs;
-		}
-		engine->executor.scheduleForLater(&tachTurnSignalOff, (int)MS2US(durationMs), (schfunc_t) &turnTachPinLow, engine);
 
+	// limit to 10..90% duty
+	duty = maxF(0.1f, minF(duty, 0.9f));
+
+	// Use duty to compute the angle widths of high/low periods
+	angle_t angleHigh = period * duty;
+	angle_t angleLow = period - angleHigh;
+
+	angle_t angle = 0;
+
+	for (int i = 0; i < periods; i++) {
+		// Rising edge
+		scheduleByAngle(&events[2 * i], angle, (schfunc_t)&setTach, &ctx_high PASS_ENGINE_PARAMETER_SUFFIX);
+		angle += angleHigh;
+
+		// Followed by falling edge
+		scheduleByAngle(&events[2 * i + 1], angle, (schfunc_t)&setTach, &ctx_low PASS_ENGINE_PARAMETER_SUFFIX);
+		angle += angleLow;
 	}
 }
 
@@ -109,13 +91,16 @@ void initTachometer(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	if (CONFIG(tachOutputPin) == GPIO_UNASSIGNED) {
 		return;
 	}
-	pulseNumbers = 1;
+
 	enginePins.tachOut.initPin("analog tach output", CONFIG(tachOutputPin), &CONFIG(tachOutputPinMode));
+
+	ctx_high.Pin = &enginePins.tachOut;
+	ctx_high.State = true;
+
+	ctx_low.Pin = &enginePins.tachOut;
+	ctx_low.State = false;
 
 #if EFI_SHAFT_POSITION_INPUT
 	addTriggerEventListener(tachSignalCallback, "tach", engine);
 #endif /* EFI_SHAFT_POSITION_INPUT */
 }
-
-
-
