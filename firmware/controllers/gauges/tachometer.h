e72001f78c00ecd6940bada00da25330176a59a9
shadowm60 <shadowm60@gmail.com>
2021-11-18 18:04:12 +0200

    Update tachometer.h

--- firmware/controllers/gauges/tachometer.h
+++ firmware/controllers/gauges/tachometer.h
@@ -5,11 +5,9 @@
  * @author Andrey Belomutskiy, (c) 2012-2020
  */
 
-#ifndef CONTROLLERS_TACHOMETER_H_
-#define CONTROLLERS_TACHOMETER_H_
+#pragma once
 
 #include "engine.h"
 
 void initTachometer();
 void tachSignalCallback();
-#endif
